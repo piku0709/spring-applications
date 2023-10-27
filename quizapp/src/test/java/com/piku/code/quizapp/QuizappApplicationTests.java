c078093d91e710cf3b440de4b17f708b3228b381
piku0709 <piku.210@gmail.com>
2023-10-27 00:26:00 -0400

    new quiz app using spring boot

---
+++ quizapp/src/test/java/com/piku/code/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piku.code.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
