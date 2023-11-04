e6f1160124988b735838fb24c993830ed650bbd2
piku0709 <piku.210@gmail.com>
2023-11-04 19:55:37 -0400

    first web app using spring boot

---
+++ MySpringBootWebApp/src/test/java/com/spring/web/app/MySpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
